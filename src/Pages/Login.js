32295fa49d62c098836aae71e6326ed600a48986
Nemancer <cddeal12@gmail.com>
2020-11-16 07:13:49 -0800

    Login page align

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,7 +3,7 @@ import LoginButton from '../Components/LoginButton';
 
 function Pages() {
     return (
-        <div className="container">
+        <div className="container text-align-center">
             <h1>Welcome to the Board Game Meetup Tracker</h1>
             <h4>Powered by the Boardgamegeek API</h4>
             <LoginButton />
